5d6a80a89cc6b541b1ef63e0d573f78d26b44a35
Brandon R <brandonramosfreire@gmail.com>
2025-02-02 09:41:41 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -93,7 +93,7 @@ function App() {
       ) : (
         <div className="login">
           <form onSubmit={handleSubmit}>
-            <span className="formTitle">Lama Login</span>
+            <span className="formTitle">Login</span>
             <input
               type="text"
               placeholder="username"
